7ebdb6a658f18315cada5f056699bee88a52d77f
Vitali177 <vskrabatun@gmail.com>
2023-04-23 19:25:58 +0300

    parsing incoming events in the query microservice

--- query/index.js
+++ query/index.js
@@ -6,14 +6,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 app.get('/posts', async (req, res) => {
-
-
+  return res.send(posts);
 });
 
 app.post('/events', (req, res) => {
-  console.log('Receiving event', req.body.type);
+  const { type, data } = req.body;
 
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
   return res.send({});
 });
 

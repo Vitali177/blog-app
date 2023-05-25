03cde98d0eba86a7372eed060cefcad02e70d738
Vitali177 <vskrabatun@gmail.com>
2023-05-25 09:02:21 +0300

    updating Service Addresses

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,14 +8,15 @@ app.use(bodyParser.json());
 const events = [];
 
 app.post('/events', (req, res) => {
+  console.log('Receiving event', req.body.type);
   const event = req.body;
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch(err => console.log(err));
-  axios.post('http://localhost:4001/events', event).catch(err => console.log(err));
-  axios.post('http://localhost:4002/events', event).catch(err => console.log(err));
-  axios.post('http://localhost:4003/events', event).catch(err => console.log(err));
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch(err => console.log(err));
+  // axios.post('http://localhost:4001/events', event).catch(err => console.log(err));
+  // axios.post('http://localhost:4002/events', event).catch(err => console.log(err));
+  // axios.post('http://localhost:4003/events', event).catch(err => console.log(err));
 
   return res.send({ status: 'OK' });
 });
